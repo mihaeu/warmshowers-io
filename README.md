7c2d6f4713a56c121b8d8c8568dbaa59b1faa5ee
Michael Haeuslmann <haeuslmann@gmail.com>
2015-05-08 23:05:41 +0200

    Setup map view. Added tabs with icons for all main views. Changed launch screen.

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Warmshowers iOS
 
 This project aims to provide a feature rich iOS client for warmshowers.org
+
+![Warmshowers iOS MapView](docs/map-mockup.png "Warmshowers iOS	")
---
+++ Warmshowers/Map/MapViewController.swift
@@ -0,0 +1,15 @@
+//
+//  MapViewController.swift
+//  Warmshowers
+//
+//  Created by Michael Haeuslmann on 08/05/15.
+//  Copyright (c) 2015 Michael Haeuslmann. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class MapViewController: UIViewController
+{
+    @IBOutlet weak var mapView: MKMapView!
+}
