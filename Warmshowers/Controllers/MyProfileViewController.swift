752803039d4d47e3d72621f47b4c263365f8be0f
Michael Haeuslmann <haeuslmann@gmail.com>
2015-05-21 02:21:41 +0200

    Started working on my profile.

---
+++ Warmshowers/Controllers/MyProfileViewController.swift
@@ -0,0 +1,24 @@
+//
+//  MyProfileViewController.swift
+//  Warmshowers
+//
+//  Created by Michael Haeuslmann on 21/05/15.
+//  Copyright (c) 2015 Michael Haeuslmann. All rights reserved.
+//
+
+import UIKit
+
+class MyProfileViewController: UIViewController
+{
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var commentsTextView: UITextView!
+    
+    var api = API()
+    
+    override func viewDidLoad()
+    {
+        // TODO: API is not a singleton so the logged in user doesn't exist on this instance
+        nameTextField.text = api.loggedInUser?.name
+        commentsTextView.text = api.loggedInUser?.comments
+    }
+}
