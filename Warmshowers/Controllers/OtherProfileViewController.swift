fcc6cd73025e6d927a69d8aae4ff1444484b1e3d
Michael Haeuslmann <haeuslmann@gmail.com>
2015-05-24 12:56:35 +0200

    Improved layout of login and other profile.

--- Warmshowers/Controllers/OtherProfileViewController.swift
+++ Warmshowers/Controllers/OtherProfileViewController.swift
@@ -15,18 +15,28 @@ class OtherProfileViewController: UIViewController
     @IBOutlet weak var userPictureImageView: UIImageView!
 
     @IBOutlet weak var userLabel: UILabel!
-    @IBOutlet weak var descriptionLabel: UILabel!
-    @IBOutlet weak var languagesSpokenLabel: UILabel!
-    
+    @IBOutlet weak var profileDescriptionTextView: UITextView!
+
     override func viewDidLoad()
     {
         if user != nil {
             userLabel.text =  user!.name
-            languagesSpokenLabel.text = user!.languagesspoken
-            descriptionLabel.text = user!.comments
+            
+            API.sharedInstance.getUser(user!.uid)
+                .onSuccess() { completeUser in
+                    self.user = completeUser
+                    self.displayUserData()
+                }
             
             let url = NSURL(string: user!.thumbnailURL)!
             userPictureImageView.hnk_setImageFromURL(url)
+        }
+    }
+    
+    private func displayUserData()
+    {
+        if user != nil {
+            profileDescriptionTextView.text = user!.comments
         }
     }
 }
