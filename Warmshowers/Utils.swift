6da3bc1936ee932a50c2e36f56b49cd44daf76c2
Michael Haeuslmann <haeuslmann@gmail.com>
2015-06-28 10:38:25 +0200

    iOS default date formats for everything.

--- Warmshowers/Utils.swift
+++ Warmshowers/Utils.swift
@@ -36,35 +36,8 @@ class Utils
     {
         let lastMessageDate = NSDate(timeIntervalSince1970: Double(timestamp))
         let dateFormatter = NSDateFormatter()
-        dateFormatter.dateFormat = "h:mm a d MMMM yyyy"
+        dateFormatter.dateStyle = .MediumStyle
+        dateFormatter.timeStyle = .ShortStyle
         return dateFormatter.stringFromDate(lastMessageDate)
-    }
-    
-    /**
-        Checks whether the phone has access to the internet or not.
-    
-        Taken from http://stackoverflow.com/questions/25398664/check-for-internet-connection-availability-in-swift
-    
-        :returns: Bool
-    */
-    static func phoneIsConnectedToNetwork() -> Bool
-    {
-        var zeroAddress = sockaddr_in(sin_len: 0, sin_family: 0, sin_port: 0, sin_addr: in_addr(s_addr: 0), sin_zero: (0, 0, 0, 0, 0, 0, 0, 0))
-        zeroAddress.sin_len = UInt8(sizeofValue(zeroAddress))
-        zeroAddress.sin_family = sa_family_t(AF_INET)
-        
-        let defaultRouteReachability = withUnsafePointer(&zeroAddress) {
-            SCNetworkReachabilityCreateWithAddress(nil, UnsafePointer($0)).takeRetainedValue()
-        }
-        
-        var flags: SCNetworkReachabilityFlags = 0
-        if SCNetworkReachabilityGetFlags(defaultRouteReachability, &flags) == 0 {
-            return false
-        }
-        
-        let isReachable = (flags & UInt32(kSCNetworkFlagsReachable)) != 0
-        let needsConnection = (flags & UInt32(kSCNetworkFlagsConnectionRequired)) != 0
-        
-        return isReachable && !needsConnection
     }
 }
